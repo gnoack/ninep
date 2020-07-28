ad2d03610d49cffd95929ef510998508e8fc54c0
Günther Noack <guenther@unix-ag.uni-kl.de>
2020-07-29 01:54:44 +0200

    stat reading and writing

--- stat.go
+++ stat.go
@@ -1,6 +1,9 @@
 package ninep
 
-import "io"
+import (
+	"errors"
+	"io"
+)
 
 // TODO: Rename this to 'Dir', to be in sync with Plan9 structs.
 type Stat struct {
@@ -10,76 +13,100 @@ type Stat struct {
 	// vector corresponding to the high 8 bits of the file's mode
 	// word.
 	// TODO: Use Qid directly here.
-	QidType uint8
-	QidVers uint32 // version number for given path
-	QidPath uint64 // the file server's unique ID for the file
-	Mode    uint32 // permissions and flags
-	Atime   uint32 // last access time
-	Mtime   uint32 // last modification time
-	Length  uint64 // length of file in bytes
-	Name    string // file name; must be / if the file is the root
-	Uid     string // owner's name
-	Gid     string // group's name
-	Muid    string // name of the user who last modified the file
+	Qid    Qid
+	Mode   uint32 // permissions and flags
+	Atime  uint32 // last access time
+	Mtime  uint32 // last modification time
+	Length uint64 // length of file in bytes
+	Name   string // file name; must be / if the file is the root
+	Uid    string // owner's name
+	Gid    string // group's name
+	Muid   string // name of the user who last modified the file
 }
 
 func readStat(r io.Reader, s *Stat) error {
-	if err := readUint16(r, &s.Type); err != nil {
+	var size uint16
+	if err := readUint16(r, &size); err != nil {
 		return err
 	}
-	if err := readUint32(r, &s.Dev); err != nil {
+	lr := &io.LimitedReader{R: r, N: int64(size)}
+	if err := readUint16(lr, &s.Type); err != nil {
 		return err
 	}
-	if err := readUint8(r, &s.QidType); err != nil {
+	if err := readUint32(lr, &s.Dev); err != nil {
 		return err
 	}
-	if err := readUint32(r, &s.QidVers); err != nil {
+	if err := readUint8(lr, &s.Qid.Kind); err != nil {
 		return err
 	}
-	if err := readUint64(r, &s.QidPath); err != nil {
+	if err := readUint32(lr, &s.Qid.Vers); err != nil {
 		return err
 	}
-	if err := readUint32(r, &s.Mode); err != nil {
+	if err := readUint64(lr, &s.Qid.Path); err != nil {
 		return err
 	}
-	if err := readUint32(r, &s.Atime); err != nil {
+	if err := readUint32(lr, &s.Mode); err != nil {
 		return err
 	}
-	if err := readUint32(r, &s.Mtime); err != nil {
+	if err := readUint32(lr, &s.Atime); err != nil {
 		return err
 	}
-	if err := readUint64(r, &s.Length); err != nil {
+	if err := readUint32(lr, &s.Mtime); err != nil {
 		return err
 	}
-	if err := readString(r, &s.Name); err != nil {
+	if err := readUint64(lr, &s.Length); err != nil {
 		return err
 	}
-	if err := readString(r, &s.Uid); err != nil {
+	if err := readString(lr, &s.Name); err != nil {
 		return err
 	}
-	if err := readString(r, &s.Gid); err != nil {
+	if err := readString(lr, &s.Uid); err != nil {
 		return err
 	}
-	if err := readString(r, &s.Muid); err != nil {
+	if err := readString(lr, &s.Gid); err != nil {
 		return err
+	}
+	if err := readString(lr, &s.Muid); err != nil {
+		return err
+	}
+	if lr.N > 0 {
+		return errors.New("stat is shorter than allocated size")
 	}
 	return nil
 }
 
+func stringSize(s string) uint16 {
+	return uint16(2 + len(s))
+}
+
+// Size of the given stat struct serialized,
+// not including the 2-byte size field.
+func statSize(s Stat) (size uint16) {
+	size += 39 // fixed part
+	size += stringSize(s.Name)
+	size += stringSize(s.Uid)
+	size += stringSize(s.Gid)
+	size += stringSize(s.Muid)
+	return size
+}
+
 func writeStat(w io.Writer, s Stat) error {
+	if err := writeUint16(w, statSize(s)); err != nil {
+		return err
+	}
 	if err := writeUint16(w, s.Type); err != nil {
 		return err
 	}
 	if err := writeUint32(w, s.Dev); err != nil {
 		return err
 	}
-	if err := writeUint8(w, s.QidType); err != nil {
+	if err := writeUint8(w, s.Qid.Kind); err != nil {
 		return err
 	}
-	if err := writeUint32(w, s.QidVers); err != nil {
+	if err := writeUint32(w, s.Qid.Vers); err != nil {
 		return err
 	}
-	if err := writeUint64(w, s.QidPath); err != nil {
+	if err := writeUint64(w, s.Qid.Path); err != nil {
 		return err
 	}
 	if err := writeUint32(w, s.Mode); err != nil {
